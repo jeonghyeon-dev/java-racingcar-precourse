e61ba08780c722d26109ebbdf003868f0fef1772
Park JeongHyeon <dev.jeonghyeon@gmail.com>
2022-04-23 22:04:22 +0900

    refactor : CarNameTest 에러 메세지 출력 테스트

--- src/test/java/racingcar/CarNameTest.java
+++ src/test/java/racingcar/CarNameTest.java
@@ -1,18 +1,30 @@
 package racingcar;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CarNameTest {
 
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
     @Test
     void 이름은_5자리_이하만_가능_합니다() {
-        
-        assertThatThrownBy(() -> {
-            CarName carName = new CarName("Park JeongHyeon");
-        }).isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(Message.NAME_CAN_NOT_BE_MORE_THEN_5_DIGITS());
+        System.setOut(new PrintStream(outputStreamCaptor));
+        CarName carName = new CarName("Park JeongHyeon");
+        assertEquals(Message.getNameCanNotBeMoreThen5Digits(), outputStreamCaptor.toString()
+                .trim());
     }
 }
