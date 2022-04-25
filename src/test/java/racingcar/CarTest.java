8f3430702b3d6e55d2eaf31566778ef4d1897353
Park JeongHyeon <dev.jeonghyeon@gmail.com>
2022-04-25 17:16:15 +0900

    refactor : Car 메소드명 수정

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -8,7 +8,6 @@ public class CarTest {
 
     @Test
     void 자동차_이름_부여() {
-
         Car car = new Car("pjh");
         assertThat(car.getName()).isEqualTo("pjh");
     }
@@ -17,9 +16,8 @@ public class CarTest {
     void 자동차_이동_테스트() {
         Car car = new Car("pjh");
         car.move(4);
-        assertThat(car.getLocation()).isEqualTo(1);
+        assertThat(car.getPosition()).isEqualTo(1);
         car.move(3);
-        assertThat(car.getLocation()).isEqualTo(1);
+        assertThat(car.getPosition()).isEqualTo(1);
     }
-
 }
