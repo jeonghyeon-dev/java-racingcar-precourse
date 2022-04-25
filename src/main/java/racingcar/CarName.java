7ec71c1b5508de875a262dd4c6a809eaffdd63ff
Park JeongHyeon <dev.jeonghyeon@gmail.com>
2022-04-25 16:26:57 +0900

    refactor : 자동차 이름 생성 테스트 코드 수정

--- src/main/java/racingcar/CarName.java
+++ src/main/java/racingcar/CarName.java
@@ -2,20 +2,20 @@ package racingcar;
 
 public class CarName {
     public static final int MAX_LENGTH = 5;
-    private final String name;
+    private String name;
 
     public CarName() {
         name = "";
     }
 
-    public CarName(String name) {
-        validationLength(name);
+    public CarName(String name) throws IllegalArgumentException {
+        validationForCarNameLength(name);
         this.name = name;
     }
 
-    private void validationLength(String name) {
-        if(name.length() > MAX_LENGTH) {
-            throw new IllegalArgumentException(Message.NAME_CAN_NOT_BE_MORE_THEN_5_DIGITS());
+    private void validationForCarNameLength(String name) {
+        if (name.length() > MAX_LENGTH) {
+            throw new IllegalArgumentException(Message.getNameCanNotBeMoreThen5Digits());
         }
     }
 
